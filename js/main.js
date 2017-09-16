ab8586b40399c80f46b8443b4e6039841ad2aa29
0xhack <f411024@gmail.com>
2017-09-17 03:10:36 +0800

    final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-window.getElementById('countdownTime').innerHTML = data
+window.onload = function() {
+  var picked = Math.floor((Math.random() * 10) + 1);
+  var btn = document.getElementById('btn_ans');
+  var ans = document.getElementById('input_ans');
+  var pic = document.getElementById('pic');
+  var txt = document.getElementById('txt');
+
+  btn.addEventListener("click",function() {
+    if ( Number(ans.value) == picked ){
+      pic.src = "img/rageface_success.png";
+    }else{
+      pic.src = "img/rageface_fail.png";
+    }
+    txt.innerHTML = "ans: " + picked;
+    setTimeout(function() {
+      pic.src = "img/rageface_info.png";
+      txt.innerHTML = "Guess what number do i think ?";
+      ans.value = null;
+    },300);
+  });
+}
